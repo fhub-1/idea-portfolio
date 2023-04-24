28fcc278d9b3cfe5d3a1515fbb841c684012a271
fhub-1 <flutterhub1.21.0@gmail.com>
2023-04-24 18:24:13 +0300

    working

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,112 +1,120 @@
 import { logo } from "@/public/assets";
 import { motion } from "framer-motion";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { HiMenuAlt3 } from "react-icons/hi";
 
 const Navbar = () => {
   return (
-    <div className="w-full shadow-md h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
-      <div className="max-w-container h-full mx-auto py-1 font-titleFonts flex items-center justify-between">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
-        >
-          <Image className="w-40" src={logo} alt="logo" />
-        </motion.div>
-        <div className="hidden mdl:inline-flex items-center gap-7 font-bodyFont">
-          <ul className="flex text-[13px] gap-7 font-bodyFont">
-            <Link
-              href="#home"
-              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
-            >
-              <motion.li
-                initial={{ y: -10, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.11 }}
-              >
-                Home
-              </motion.li>
+    <>
+      <Head>
+        <title>Navbar</title>
+      </Head>
+      <div className="w-full shadow-md h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
+        <div className="max-w-container h-full mx-auto py-1 font-titleFonts flex items-center justify-between">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Link href="/">
+              <Image className="w-40" src={logo} alt="logo" />
             </Link>
-            <Link
-              href="#about"
-              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
-            >
-              <motion.li
-                initial={{ y: -10, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.1, delay: 0.12 }}
+          </motion.div>
+          <div className="hidden mdl:inline-flex items-center gap-7 font-bodyFont">
+            <ul className="flex text-[13px] gap-7 font-bodyFont">
+              <Link
+                href="#home"
+                className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
               >
-                <span className="text-textGreen">01.</span>About
-              </motion.li>
-            </Link>
-            <Link
-              href="#experience"
-              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
-            >
-              <motion.li
-                initial={{ y: -10, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.1, delay: 0.21 }}
+                <motion.li
+                  initial={{ y: -10, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.11 }}
+                >
+                  Home
+                </motion.li>
+              </Link>
+              <Link
+                href="#about"
+                className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
               >
-                <span className="text-textGreen">02.</span>Experience
-              </motion.li>
-            </Link>
-            <Link
-              href="#project"
-              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
-            >
-              <motion.li
-                initial={{ y: -10, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.1, delay: 0.31 }}
+                <motion.li
+                  initial={{ y: -10, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.1, delay: 0.12 }}
+                >
+                  <span className="text-textGreen">01.</span>About
+                </motion.li>
+              </Link>
+              <Link
+                href="#experience"
+                className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
               >
-                <span className="text-textGreen">03.</span>Project
-              </motion.li>
-            </Link>
-            <Link
-              href="#Blog"
-              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
-            >
-              <motion.li
-                initial={{ y: -10, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.1, delay: 0.41 }}
+                <motion.li
+                  initial={{ y: -10, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.1, delay: 0.21 }}
+                >
+                  <span className="text-textGreen">02.</span>Experience
+                </motion.li>
+              </Link>
+              <Link
+                href="#project"
+                className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
               >
-                <span className="text-textGreen">04.</span>Blog
-              </motion.li>
-            </Link>
-            <Link
-              href="#contact"
-              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
-            >
-              <motion.li
-                initial={{ y: -10, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.1, delay: 0.51 }}
+                <motion.li
+                  initial={{ y: -10, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.1, delay: 0.31 }}
+                >
+                  <span className="text-textGreen">03.</span>Project
+                </motion.li>
+              </Link>
+              <Link
+                href="#Blog"
+                className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
               >
-                <span className="text-textGreen">05.</span>Contact
-              </motion.li>
-            </Link>
-          </ul>
-          <a href="/assets/resume.pdf" target="_blank">
-            <motion.button
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 0.61 }}
-              className="px-4 py-2 rounded-md text-textGreen text-[13px] border border-textGreen  hover:bg-hoverColor duration-300"
-            >
-              Resume
-            </motion.button>
-          </a>
-        </div>
-        {/* menu icons */}
-        <div className="w-6 -h-5 flex flex-col justify-between items-center mdl:hidden text-4xl text-textGreen cursor-pointer overflow-hidden group">
-          <HiMenuAlt3 />
+                <motion.li
+                  initial={{ y: -10, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.1, delay: 0.41 }}
+                >
+                  <span className="text-textGreen">04.</span>Blog
+                </motion.li>
+              </Link>
+              <Link
+                href="#contact"
+                className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+              >
+                <motion.li
+                  initial={{ y: -10, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.1, delay: 0.51 }}
+                >
+                  <span className="text-textGreen">05.</span>Contact
+                </motion.li>
+              </Link>
+            </ul>
+            <a href="/assets/resume.pdf" target="_blank">
+              <motion.button
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.61 }}
+                className="px-4 py-2 rounded-md text-textGreen text-[13px] border border-textGreen  hover:bg-hoverColor duration-300"
+              >
+                Resume
+              </motion.button>
+            </a>
+          </div>
+          {/* menu icons */}
+          <div className="w-6 -h-5 flex flex-col justify-between items-center mdl:hidden text-4xl text-textGreen cursor-pointer overflow-hidden group">
+            <HiMenuAlt3 />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,39 +6,45 @@ import Navbar from "@/components/Navbar";
 import Project from "@/components/Project";
 import RightSide from "@/components/RightSide";
 import { motion } from "framer-motion";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
-      <Navbar />
-      <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1.5 }}
-          className="hidden xl:inline-flex w-32  h-full fixed left-0 bottom-0"
-        >
-          <LeftSIder />
-        </motion.div>
-        <div className="h-[88vh] w-full mx-auto p-4">
-          <Banner />
-          {/* about */}
-          <About />
-          {/* exprince */}
-          <Experience />
-          {/* project */}
-          <Project />
-          {/* footer */}
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+        <Navbar />
+        <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32  h-full fixed left-0 bottom-0"
+          >
+            <LeftSIder />
+          </motion.div>
+          <div className="h-[88vh] w-full mx-auto p-4">
+            <Banner />
+            {/* about */}
+            <About />
+            {/* exprince */}
+            <Experience />
+            {/* project */}
+            <Project />
+            {/* footer */}
+          </div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
+          >
+            <RightSide />
+          </motion.div>
         </div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1.5 }}
-          className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
-        >
-          <RightSide />
-        </motion.div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
